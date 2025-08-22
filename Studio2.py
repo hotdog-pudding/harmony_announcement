e589481f260bb60dfaf1bd43cba5fb602e743dc8
hotdog-pudding <45160403+hotdog-pudding@users.noreply.github.com>
2025-08-22 12:29:34 -0700

    Add files via upload

--- Studio2.py
+++ Studio2.py
@@ -1,7 +1,7 @@
 import my_functions
     
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+with my_functions.socket.socket(my_functions.socket.AF_INET, my_functions.socket.SOCK_STREAM) as s:
+    s.bind((my_functions.HOST, my_functions.PORT))
     s.listen()
     while True:
         conn, addr = s.accept()
@@ -11,39 +11,39 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 if not data:
                     break
                 studentName = data.decode("utf-8").title()
-                studentName = typoCheck(studentName)
+                studentName = my_functions.typoCheck(studentName)
                 match studentName:
                     case "Luke B":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Daniel C":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Aeneas S":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Jacob S":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Sophia T":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Nicholas Y":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Noah C":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Violet H":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Jolin H":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Karsten L":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Jacob L":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Oliver L":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Victoria R":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "William T":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Abigail C":
-                        announcement(studentName)
+                        my_functions.announcement(studentName)
                     case "Bianca R":
-                        announcement(studentName)
-                failReply(studentName)
+                        my_functions.announcement(studentName)
+                my_functions.failReply(studentName)
                 
