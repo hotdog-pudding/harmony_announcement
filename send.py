1a8f8172e8157c5622224dc31980e741395fe533
Pudding <mrblaineng@gmail.com>
2025-09-04 13:55:36 -0700

    Sends to Zone1

--- send.py
+++ send.py
@@ -4,12 +4,22 @@ from my_functions import typoCheck
 
 
 while True:    
+    studentName = input("Student: ").title()
+    studentName = typoCheck(studentName)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        studentName = input("Student: ").title()
-        studentName = typoCheck(studentName)
         for key in roster.rosterWithClass:
             if studentName == key:
                 s.connect((roster.rosterWithClass[studentName], roster.PORT))
+                name_bytes = studentName.encode("utf-8")
+                s.sendall(name_bytes)
+                print(f"{studentName} at {roster.rosterWithClass[studentName]}")
+                data = s.recv(1024)
+                received_response = data.decode("utf-8")
+                print(received_response)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        for key in roster.rosterWithClass:
+            if studentName == key:
+                s.connect((roster.classroomIP['Zone1'], roster.PORT))
                 name_bytes = studentName.encode("utf-8")
                 s.sendall(name_bytes)
                 print(f"{studentName} at {roster.rosterWithClass[studentName]}")
