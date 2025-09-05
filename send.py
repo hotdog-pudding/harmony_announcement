85ae58e9ac0d611198fa0e6d3f477704b9559991
pudding <mrblaineng@gmail.com>
2025-09-05 01:36:49 -0700

    Added try block to prevent timeout

--- send.py
+++ send.py
@@ -1,28 +1,42 @@
 import socket
 import roster
-from my_functions import typoCheck
+import my_functions
 
 
 while True:    
     studentName = input("Student: ").title()
-    studentName = typoCheck(studentName)
+    my_functions.printSeparator()
+    studentName = my_functions.typoCheck(studentName)
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(2.0)
         for key in roster.rosterWithClass:
             if studentName == key:
-                s.connect((roster.rosterWithClass[studentName], roster.PORT))
-                name_bytes = studentName.encode("utf-8")
-                s.sendall(name_bytes)
-                print(f"{studentName} at {roster.rosterWithClass[studentName]}")
-                data = s.recv(1024)
-                received_response = data.decode("utf-8")
-                print(received_response)
+                try:
+                    s.connect((roster.rosterWithClass[studentName], roster.PORT))
+                    name_bytes = studentName.encode("utf-8")
+                    s.sendall(name_bytes)
+                    my_functions.sendMsg(studentName)
+                    data = s.recv(1024)
+                    received_response = data.decode("utf-8")
+                    print(received_response)
+                    my_functions.printSeparator()
+                except:
+                    print(f'{studentName}')
+                    print(my_functions.timeOutMsg(studentName))
+                    my_functions.printSeparator()
+                    continue
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(2.0)
         for key in roster.rosterWithClass:
             if studentName == key:
-                s.connect((roster.classroomIP['Zone1'], roster.PORT))
-                name_bytes = studentName.encode("utf-8")
-                s.sendall(name_bytes)
-                print(f"{studentName} at {roster.rosterWithClass[studentName]}")
-                data = s.recv(1024)
-                received_response = data.decode("utf-8")
-                print(received_response)
+                try:
+                    s.connect((roster.classroomIP['Zone1'], roster.PORT))
+                    name_bytes = studentName.encode("utf-8")
+                    s.sendall(name_bytes)
+                    data = s.recv(1024)
+                    received_response = data.decode("utf-8")
+                except:
+                    print(f'{studentName}')
+                    print(f'Unable to connect to outside zone')
+                    my_functions.printSeparator()
