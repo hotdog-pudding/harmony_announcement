f86d6732550f52cce6504eead8c25707edcba24f
pudding <mrblaineng@gmail.com>
2025-09-05 01:37:22 -0700

    Sends to all classrooms being used on Friday

--- sendAll.py
+++ sendAll.py
@@ -1,18 +1,49 @@
 import socket
 import roster
-from my_functions import typoCheck
+import my_functions
 
-while True:    
+while True:
+    studentName = input("Student: ").title()
+    my_functions.printSeparator()
+    studentName = my_functions.typoCheck(studentName)
+    for key in roster.rosterWithClass:
+        if studentName == key:
+            for value in roster.fridayClassroomIP.values():
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.settimeout(2.0)
+                    try:
+                        s.connect((value, roster.PORT))
+                        name_bytes = studentName.encode("utf-8")
+                        s.sendall(name_bytes)
+                        print(f'Sending {studentName} to {value}')
+                        data = s.recv(1024)
+                        received_response = data.decode("utf-8")
+                        #print(received_response)
+                    except:
+                        print(f'Unable to connect to {value}')
+                        #print(value)
+                        continue
+            my_functions.printSeparator()
+'''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        studentName = input("").title()
-        studentName = typoCheck(studentName)
+        s.settimeout(2.0)
         for key in roster.rosterWithClass:
             if studentName == key:
                 for value in roster.classroomIP.values():
-                    s.connect((value, roster.PORT))
-                    name_bytes = studentName.encode("utf-8")
-                    s.sendall(name_bytes)
-                    print(f"{studentName} at {roster.rosterWithClass[studentName]}")
-                    data = s.recv(1024)
-                    received_response = data.decode("utf-8")
-                    print(received_response)
+                    try:
+                        print(value)
+                        s.connect((value, roster.PORT))
+                        print('connected')
+                        name_bytes = studentName.encode("utf-8")
+                        s.sendall(name_bytes)
+                        my_functions.sendMsg(studentName)
+                        data = s.recv(1024)
+                        received_response = data.decode("utf-8")
+                        print(received_response)
+                    except:
+                        print('fail')
+                        #print(value)
+                        #print(my_functions.timeOutMsg(studentName))
+                        continue
+                        '''
+
