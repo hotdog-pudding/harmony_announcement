300e8e6973e206f63c64aea24ae6cbe6db61a7bb
pudding <mrblaineng@gmail.com>
2025-10-23 00:13:22 -0700

    Sockets are on a thread now

--- send.py
+++ send.py
@@ -1,15 +1,28 @@
 import socket
 import roster
 import my_functions
+import threading
 
+
+def connect_to_server(host, port, studentName):
+        try:
+            with socket.create_connection((host,port), timeout=10) as s:
+                s.sendall(bytes(studentName, encoding='utf-8'))
+        except Exception as e:
+            print(f"{studentName} Unable to connect to {host}")
+
+def find_classroom_ip(studentName):
+     for key in roster.rosterWithClass:
+          if studentName == key:
+               return roster.rosterWithClass[studentName]
 
 while True:    
     studentName = input("Student: ").title()
     my_functions.printSeparator()
     studentName = my_functions.typoCheck(studentName)
-    
+    '''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(0.5)
+        #s.settimeout(0.5)
         for key in roster.rosterWithClass:
             if studentName == key:
                 try:
@@ -17,23 +30,32 @@ while True:
                     name_bytes = studentName.encode("utf-8")
                     s.sendall(name_bytes)
                     my_functions.studentFoundReply(studentName)
-                    data = s.recv(1024)
-                    received_response = data.decode("utf-8")
-                    print(received_response)
+                    #data = s.recv(1024)
+                    #received_response = data.decode("utf-8")
+                    #print(received_response)
                 except:
                     print(f'{studentName}')
                     print(my_functions.timeOutMsg(studentName))
                     continue
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(2.0)
+        #s.settimeout(2.0)
         for key in roster.rosterWithClass:
             if studentName == key:
                 try:
                     s.connect((roster.classroomIP['Zone1'], roster.PORT))
                     name_bytes = studentName.encode("utf-8")
                     s.sendall(name_bytes)
-                    data = s.recv(1024)
-                    received_response = data.decode("utf-8")
+                    #data = s.recv(1024)
+                    #received_response = data.decode("utf-8")
                 except:
                     print(f'Unable to connect to Zone 1 (192.168.1.18)')
     my_functions.printSeparator()
+    '''
+
+    servers = [("192.168.1.18", 2360)]
+    servers.append((find_classroom_ip(studentName), 2360))
+    threads = []
+    for host, port in servers:
+         t = threading.Thread(target=connect_to_server, args=(host, port, studentName))
+         t.start()
+         threads.append(t)
